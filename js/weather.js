b159b1ac7b30061b2881936f09c50e2f1b051bbb
Palash Mandal <pmpalashmandal@hmail.com>
2021-08-28 18:15:23 +0600

    first commit

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+
+const loadWeather = () =>{
+    const inputBox = document.getElementById('input-box');
+    const inputCity = inputBox.value;
+    inputBox.value = '';
+    const api = '7e99a06886a8fd0bc394e38de6dbc0fe';
+    
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${api}`)
+    .then(res => res.json())
+    .then(data => displayWeather(data));
+    
+};
+
+const displayWeather = data =>{
+    // console.log(data);
+    const city = document.getElementById('city-name');
+    const temperature = document.getElementById('temp');
+    const description = document.getElementById('description');
+    const temp = (data.main.temp-273.15).toFixed(2);
+
+    city.innerText = `${data.name}`;
+    temperature.innerText = `${temp}`;
+    description.innerText = `${data.weather[0].main}`;
+}
