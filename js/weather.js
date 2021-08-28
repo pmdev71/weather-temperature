ab5fc8528887a88916e29362243bc5c8a13be09e
Palash Mandal <pmpalashmandal@hmail.com>
2021-08-28 20:54:33 +0600

    first commit

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,16 @@
 
-const loadWeather = () =>{
+const loadWeather = async() =>{
     const inputBox = document.getElementById('input-box');
     const inputCity = inputBox.value;
     inputBox.value = '';
     const api = '7e99a06886a8fd0bc394e38de6dbc0fe';
-    
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${api}`)
+
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${api}`);
+    const data = await res.json();
+    displayWeather(data);
+    /* fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${api}`)
     .then(res => res.json())
-    .then(data => displayWeather(data));
+    .then(data => displayWeather(data)); */
     
 };
 
@@ -21,4 +24,4 @@ const displayWeather = data =>{
     city.innerText = `${data.name}`;
     temperature.innerText = `${temp}`;
     description.innerText = `${data.weather[0].main}`;
-}
+};
